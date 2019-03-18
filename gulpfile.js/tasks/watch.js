6f036189d3c0bd0a5402d1c68715fdbf28dac397
dmitry.bndar <dmitry.bndar@gmail.com>
2019-03-18 17:36:18 +0200

    fixed gulp watch task

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,27 +1,56 @@
-const {
-  gulp,
-  bs
-} = $;
+const { gulp, bs } = $;
 
-gulp.task('watch', function () {
-  gulp.watch(process.env.WATCH_SCSS, {
-    delay: 1000
-  }, gulp.series('styles'));
-  gulp
-    .watch(process.env.WATCH_HTML, {
+const browserSyncReload = done => {
+  bs.reload();
+  done();
+};
+
+gulp.task('watch', function() {
+  gulp.watch(
+    process.env.WATCH_SCSS,
+    {
       delay: 1000
-    }, gulp.series('html', bs.reload));
-  gulp
-    .watch(process.env.WATCH_JS, {
+    },
+    gulp.series('styles')
+  );
+
+  gulp.watch(
+    process.env.WATCH_HTML,
+    {
       delay: 1000
-    }, gulp.series('scripts', bs.reload));
-  gulp.watch(process.env.WATCH_IMG, {
-    delay: 1000
-  }, gulp.series('img'));
-  gulp.watch(process.env.WATCH_STATIC, {
-    delay: 1000
-  }, gulp.series('copy'));
-  gulp.watch(process.env.WATCH_SVG, {
-    delay: 1000
-  }, gulp.series('svg'));
-})
+    },
+    gulp.series('html', browserSyncReload)
+  );
+
+  gulp.watch(
+    process.env.WATCH_JS,
+    {
+      delay: 1000
+    },
+    gulp.series('scripts', browserSyncReload)
+  );
+
+  gulp.watch(
+    process.env.WATCH_IMG,
+    {
+      delay: 1000
+    },
+    gulp.series('img')
+  );
+
+  gulp.watch(
+    process.env.WATCH_STATIC,
+    {
+      delay: 1000
+    },
+    gulp.series('copy')
+  );
+
+  gulp.watch(
+    process.env.WATCH_SVG,
+    {
+      delay: 1000
+    },
+    gulp.series('svg')
+  );
+});
