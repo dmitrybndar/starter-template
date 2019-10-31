02ac0002c53947d302768fa3cf913a94c91039ce
dmitrybondar <dmitry.bndar@gmail.com>
2019-10-31 10:56:18 +0200

    enchanced page-header

--- src/js/header.js
+++ src/js/header.js
@@ -1,11 +1,30 @@
 (function() {
   const $header = $('.page-header');
 
-  const sticky = $header.offset().top + $header.height();
+  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
   function stickyHeader() {
-    $header.toggleClass('sticky', window.pageYOffset > sticky);
+    const headerOffset = 0;
+
+    const stickyPoint = headerOffset + $('.page-header').height();
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+      // downscroll code
+      $header.removeClass('scrolled--visible');
+    } else {
+      // upscroll code
+      $header.addClass('scrolled--visible');
+    }
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+
+    $header.toggleClass('scrolled', window.pageYOffset > stickyPoint);
+
+    if (window.pageYOffset <= stickyPoint) {
+      $header.removeClass('scrolled--visible scrolled');
+    }
   }
 
-  $(window).on('scroll', stickyHeader);
+  $(window).on('scroll load', stickyHeader);
 })();
